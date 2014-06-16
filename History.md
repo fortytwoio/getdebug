3cbd82df35f3228d78c70920612bcb60bf830b2c
Thomas Fritz <fritztho@gmail.com>
2014-06-16 18:51:58 +0200

    Automatically detect application name and application root. Posibility to override with env vars

--- History.md
+++ History.md
@@ -1,4 +1,9 @@
 
+2.0.0 / 2014-06-16 
+==================
+
+ * Now application name and application root gets automatically detected. Can be overriden by setting APP_NAME and APP_ROOT environment vars
+
 1.0.3 / 2014-06-13 
 ==================
 
--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 var debugModule = require("debug");
 var path = require("path");
 
-exports = module.exports = function getDebug(applicationName, absolutePath) {
-    return function (namespace) {
-        var relativePath = path.relative(absolutePath, namespace);
-        var extname = path.extname(relativePath);
-        var identifier = path.basename(relativePath, extname);
+var applicationName = "";
+var applicationRootPath;
+var addPid = true;
 
-        var debugParts = path.dirname(relativePath).split(path.sep);
-        debugParts.push(identifier);
-        var relativeIdentifier = debugParts.join(":");
-        return debugModule(applicationName + "(" + process.pid + "):" + relativeIdentifier);
+if (process.env.APP_ROOT) {
+    applicationRootPath = process.env.APP_ROOT;
+}
+else {
+    applicationRootPath = process.cwd();
+}
+
+
+if (process.env.APP_NAME) {
+    applicationName = process.env.APP_NAME;
+}
+else {
+    applicationName = path.basename(applicationRootPath);
+}
+
+if (process.env.DEBUG_PID) {
+    var dpid = process.env.DEBUG_PID.toLowerCase();
+    if (dpid === "yes" || dpid === "true" || dpid === "y") {
+        addPid = true;
     }
-};
+    else {
+        addPid = false;
+    }
+}
+
+exports = module.exports = function getDebug(namespace) {
+    var relativePath = path.relative(applicationRootPath, namespace);
+    var extname = path.extname(relativePath);
+    var identifier = path.basename(relativePath, extname);
+
+    var debugParts = path.dirname(relativePath).split(path.sep);
+    debugParts.push(identifier);
+    var relativeIdentifier = debugParts.join(":");
+    var processIdString = "";
+    if (addPid) {
+        processIdString = "(" + process.pid + ")";
+    }
+    return debugModule(applicationName + processIdString + ":" + relativeIdentifier);
+}
